bc919b999a7a631de8c5e958ee7a70596523e917
Wenceslao Castillo <josecastillo.globant@latam.com>
2022-01-21 13:37:04 -0300

    add datadog output

--- run-load-test.sh
+++ run-load-test.sh
@@ -1,4 +1,4 @@
-docker compose up -d influxdb grafana
+docker compose up -d influxdb grafana datadog
 echo "--------------------------------------------------------------------------------------"
 echo "Load testing with Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
 echo "--------------------------------------------------------------------------------------"
